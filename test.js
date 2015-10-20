ee7e22fa90e9a73581f8ae1cec7f3ae543e04c9d
Peter Moresi <peter.moresi@gmail.com>
2015-10-19 22:08:32 -0700

    Add test

---
+++ test.js
@@ -0,0 +1,10 @@
+import test from 'tape'
+
+test('it be an exclusive and', (t) => {
+  t.plan(5)
+  t.equal( and(true, true), true)
+  t.equal( and(true, false), false)
+  t.equal( and(false, true), false)
+  t.equal( and(true, true, true), true)
+  t.equal( and(true, false, true), false)
+});
